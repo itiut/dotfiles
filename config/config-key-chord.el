3367f6202c04dc5e31eb4c406ad0651f4ac2aeee
Yuichi Tanikawa <t.yuichi111@gmail.com>
2012-10-24 01:20:08 +0900

    Delete binding a+h in key-chord

--- config/config-key-chord.el
+++ config/config-key-chord.el
@@ -3,7 +3,6 @@
 (setq key-chord-two-keys-delay 0.04)
 
 (key-chord-define-global "jk" 'view-mode)
-(key-chord-define-global "ha" 'apropos-command)
 (key-chord-define-global "hb" 'describe-bindings)
 (key-chord-define-global "hf" 'describe-function)
 (key-chord-define-global "hk" 'describe-key)
