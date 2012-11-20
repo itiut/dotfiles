9cbaac646576e395ec1b226b282fbb36f323e2a7
Yuichi Tanikawa <t.yuichi111@gmail.com>
2012-11-20 09:42:34 +0900

    Use autoload instead of require

--- config/config-zencoding-mode.el
+++ config/config-zencoding-mode.el
@@ -1,5 +1,5 @@
 ;;; zencoding-mode
-(require 'zencoding-mode)
+(autoload 'zencoding-mode "zencoding-mode" nil t)
 
 (define-key zencoding-mode-keymap (kbd "C-j") (lookup-key global-map (kbd "C-j")))
 (define-key zencoding-mode-keymap (kbd "C-,") 'zencoding-expand-yas)
