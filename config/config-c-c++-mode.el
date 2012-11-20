e66a33fd350bbed42f0685008904fbc5fdae24cf
Yuichi Tanikawa <t.yuichi111@gmail.com>
2012-11-20 10:56:29 +0900

    Rename funciton and delete comments

--- config/config-c-c++-mode.el
+++ config/config-c-c++-mode.el
@@ -1,5 +1,5 @@
 ;;; c-mode, c++-mode
-(defun my-c-c++-mode-hook ()
+(defun my-c-c++-mode-setup ()
   (c-set-style "stroustrup")
   (c-set-offset 'innamespace 0)
   (c-set-offset 'arglist-close 0)
@@ -7,15 +7,11 @@
   ;; flymake
   (flymake-mode 1)
 
-  ;; (when (require 'c-eldoc nil t)
-  ;;   (set (make-local-variable 'eldoc-idle-delay) 0.2)
-  ;;   (c-turn-on-eldoc-mode))
-
   ;; @name      yasnppet
   ;; @brief     スニペット機能
   ;; @install   http://code.google.com/p/yasnippet/downloads/list からダウンロード
   ;; (require 'yasnippet-bundle))
   )
 
-(add-hook 'c-mode-hook 'my-c-c++-mode-hook)
-(add-hook 'c++-mode-hook 'my-c-c++-mode-hook)
+(add-hook 'c-mode-hook 'my-c-c++-mode-setup)
+(add-hook 'c++-mode-hook 'my-c-c++-mode-setup)
