08beddb8e37705806a763df02aa88b43c263afa7
Yuichi Tanikawa <t.yuichi111@gmail.com>
2012-11-28 08:42:10 +0900

    Delete compile option -lm

--- config/config-smart-compile.el
+++ config/config-smart-compile.el
@@ -4,8 +4,8 @@
 
 (setq smart-compile-alist
       (append '(
-                ("\\.c\\'"          . "gcc -Wall -Wextra -O2 -std=gnu99 %f -lm")
-                ("\\.[Cc]+[Pp]*\\'" . "g++ -Wall -Wextra -O2 %f -lm")
+                ("\\.c\\'"          . "gcc -Wall -Wextra -O2 -std=gnu99 %f")
+                ("\\.[Cc]+[Pp]*\\'" . "g++ -Wall -Wextra -O2 %f")
                 ("\\.jade\\'"       . "jade %f")
                 ("\\.styl\\'"       . "stylus %f")
                 ) smart-compile-alist))
