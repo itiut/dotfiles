48826e5fa6f19df0271fb0df5704b945ae0a44aa
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-02-03 23:36:15 +0900

    Change node path for nodebrew

--- config/config-js-comint.el
+++ config/config-js-comint.el
@@ -1,7 +1,7 @@
 ;;; js-comint
 (require 'js-comint)
 
-(setq inferior-js-program-command "node")
+(setq inferior-js-program-command "~/.nodebrew/current/bin/node")
 
 (add-hook 'inferior-js-mode-hook
           '(lambda ()
