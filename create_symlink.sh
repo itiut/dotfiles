91b885c21495580816ea61b96c5810636891fb70
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-02-18 10:58:56 +0900

    Add create symlink script

---
+++ create_symlink.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+cd $(dirname $0)
+for dotfile in .?*; do
+    case $dotfile in
+        ..)
+            continue;;
+        .git)
+            continue;;
+        .gitignore)
+            continue;;
+        .gitmodules)
+            continue;;
+        *)
+            ln -Fis "$PWD/$dotfile" $HOME
+            ;;
+    esac
+done
