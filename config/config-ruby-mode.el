3fd4846b30099e91d3825c8f9a225c774f589685
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-05-30 01:48:53 +0900

    Open Gemfile in ruby-mode

--- config/config-ruby-mode.el
+++ config/config-ruby-mode.el
@@ -1,4 +1,6 @@
 ;;; ruby-mode
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+
 (add-hook 'ruby-mode-hook
           '(lambda ()
              (add-to-list 'ac-sources 'ac-source-rsense)))
