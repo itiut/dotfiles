f96de2126d935304e28bf4f588fcf6409c185aaf
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-05-30 20:16:29 +0900

    Change c-eldoc.rcp

--- config/config-c-eldoc.el
+++ config/config-c-eldoc.el
@@ -1,8 +1,5 @@
 ;;; c-eldoc
 (require 'c-eldoc)
-(defun my-c-eldoc-setup ()
-  (set (make-local-variable 'eldoc-idle-delay) 0.20)
-  (c-turn-on-eldoc-mode))
 
-(add-hook 'c-mode-hook 'my-c-eldoc-setup)
-(add-hook 'c++-mode-hook 'my-c-eldoc-setup)
+(add-hook 'c-mode-hook 'c-turn-on-eldoc-mode)
+(add-hook 'c++-mode-hook 'c-turn-on-eldoc-mode)
