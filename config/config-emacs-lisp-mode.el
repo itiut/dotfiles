6d29b310316ba12725a7550f3a4a26ed834a2d70
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-05-30 20:06:36 +0900

    Fix eldoc-mode is too heavy to scrolling

--- config/config-emacs-lisp-mode.el
+++ config/config-emacs-lisp-mode.el
@@ -1,6 +1,2 @@
 ;;; emacs-lisp-mode
-(defun my-emacs-lisp-mode-setup ()
-  (set (make-local-variable 'eldoc-idle-delay) 0.20)
-  (turn-on-eldoc-mode))
-
-(add-hook 'emacs-lisp-mode-hook 'my-emacs-lisp-mode-setup)
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
