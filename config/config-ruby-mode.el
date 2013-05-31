fc1e950a6fecea2640be15883675267e9bdd29fd
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-05-31 23:14:08 +0900

    Disable auto insertion of magic comments

--- config/config-ruby-mode.el
+++ config/config-ruby-mode.el
@@ -1,6 +1,8 @@
 ;;; ruby-mode
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 
+(setq ruby-insert-encoding-magic-comment nil)
+
 (add-hook 'ruby-mode-hook
           '(lambda ()
              (add-to-list 'ac-sources 'ac-source-rsense)))
