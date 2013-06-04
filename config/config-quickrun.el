962010b9d537209a2bffd1aa867c347a4bc25fe3
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-06-04 15:38:11 +0900

    Add quickrun-command c/gnu99 and c++/g++

--- config/config-quickrun.el
+++ config/config-quickrun.el
@@ -9,3 +9,16 @@
   (if (region-active-p)
       (quickrun :start (region-beginning) :end (region-end))
     (quickrun)))
+
+(quickrun-add-command "c/gnu99"
+                      '((:command . "gcc -std=gnu99")
+                        (:exec . ("%c %o -o %e %s"
+                                  "%e %a"))
+                        (:compile-only . "%c -Wall -Wextra -Werror %o -o %e %s")
+                        (:remove . "%e")
+                        (:description . "Compile C file with gcc (GNU99) and execute"))
+                      :default "c")
+
+(quickrun-add-command "c++/g++"
+                      '((:compile-only . "%c -Wall -Wextra -Werror %o -o %e %s"))
+                      :override t)
