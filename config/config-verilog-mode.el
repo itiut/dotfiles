c4c85710ea536abe5d4969e0ed0b30c70ee6e737
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-07-01 15:24:59 +0900

    Use autoload in verilog-mode

--- config/config-verilog-mode.el
+++ config/config-verilog-mode.el
@@ -1,17 +1,19 @@
 ;;; verilog-mode
-(defun my-verilog-mode-setup ()
-  (setq verilog-indent-level             4
-        verilog-indent-level-module      4
-        verilog-indent-level-declaration 4
-        verilog-indent-level-behavioral  4
-        verilog-indent-level-directive   4
-        verilog-case-indent              4
-        verilog-auto-newline             nil
-        verilog-auto-indent-on-newline   nil
-        verilog-tab-always-indent        t
-        verilog-auto-endcomments         t
-        verilog-minimum-comment-distance 40
-        verilog-indent-begin-after-if    t
-        verilog-auto-lineup              '(all)))
+(autoload 'verilog-mode "verilog-mode" "Verilog mode" t)
+(add-to-list 'auto-mode-alist '("\\.v\\'" . verilog-mode))
 
-(add-hook 'verilog-mode-hook 'my-verilog-mode-setup)
+(eval-after-load "verilog-mode"
+  '(progn
+     (setq verilog-indent-level             4
+           verilog-indent-level-module      4
+           verilog-indent-level-declaration 4
+           verilog-indent-level-behavioral  4
+           verilog-indent-level-directive   4
+           verilog-case-indent              4
+           verilog-auto-newline             nil
+           verilog-auto-indent-on-newline   nil
+           verilog-tab-always-indent        t
+           verilog-auto-endcomments         t
+           verilog-minimum-comment-distance 40
+           verilog-indent-begin-after-if    t
+           verilog-auto-lineup              '(all))))
