a39bac5868f528ffb64d43a197bc6257918f2b8e
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-08-26 15:11:22 +0900

    Open *.gemspec in ruby-mode

--- config/config-ruby-mode.el
+++ config/config-ruby-mode.el
@@ -1,5 +1,6 @@
 ;;; ruby-mode
 (add-to-list 'auto-mode-alist '("\\Gemfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec\\'" . ruby-mode))
 
 (setq ruby-deep-indent-paren nil)
 (setq ruby-deep-indent-paren-style nil)
