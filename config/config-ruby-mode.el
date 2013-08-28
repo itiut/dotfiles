ffb6108fc58a666abc4acc18944c114104bb4647
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-08-28 20:42:58 +0900

    Open *.ru in ruby-mode

--- config/config-ruby-mode.el
+++ config/config-ruby-mode.el
@@ -2,6 +2,7 @@
 (add-to-list 'auto-mode-alist '("\\Gemfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Rakefile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
 
 (setq ruby-deep-indent-paren nil)
 (setq ruby-deep-indent-paren-style nil)
