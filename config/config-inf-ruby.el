3837e7c46071fe9e2b34271cdcbed351829f4447
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2013-11-09 20:08:09 +0900

    deprecated

--- config/config-inf-ruby.el
+++ config/config-inf-ruby.el
@@ -1,6 +1,2 @@
 ;;; inf-ruby
 (autoload 'inf-ruby "inf-ruby" "Run an inferior Ruby process" t)
-(autoload 'inf-ruby-setup-keybindings "inf-ruby" "" t)
-
-(eval-after-load "ruby-mode"
-  '(add-hook 'ruby-mode-hook 'inf-ruby-setup-keybindings))
