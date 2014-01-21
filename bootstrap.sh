493c20fc60162ebc8efa9ce6268612ff5a9ba0c0
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-01-22 05:22:09 +0900

    extract function: link_dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,25 @@
 #!/bin/sh -eu
+
+link_dotfiles() {
+    echo ""
+    echo "link dotfiles..."
+
+    for dotfile in .?*; do
+        case $dotfile in
+            ..)
+                continue;;
+            .git)
+                continue;;
+            .gitignore)
+                continue;;
+            .gitmodules)
+                continue;;
+            *)
+                ln -is "$PWD/$dotfile" $HOME
+                ;;
+        esac
+    done
+}
+
 cd $(dirname $0)
-for dotfile in .?*; do
-    case $dotfile in
-        ..)
-            continue;;
-        .git)
-            continue;;
-        .gitignore)
-            continue;;
-        .gitmodules)
-            continue;;
-        *)
-            ln -is "$PWD/$dotfile" $HOME
-            ;;
-    esac
-done
+link_dotfiles
