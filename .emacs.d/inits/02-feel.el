50d7b4d3cb9eb47c462844f7b16d8ce9454f7777
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-02-01 15:04:20 +0900

    region-based isearch

--- .emacs.d/inits/02-feel.el
+++ .emacs.d/inits/02-feel.el
@@ -52,3 +52,17 @@
   (let ((current-prefix-arg 1))
     (call-interactively 'previous-line))
   (indent-according-to-mode))
+
+;;; advices
+;; region based isearch when region is active
+(defadvice isearch-mode (around isearch-mode-default-string (forward &optional regexp op-fun recursive-edit word-p) activate)
+  (if (and transient-mark-mode mark-active (not (eq (mark) (point))))
+      (progn
+        (isearch-update-ring (buffer-substring-no-properties (mark) (point)))
+        (deactivate-mark)
+        ad-do-it
+        (if (not forward)
+            (isearch-repeat-backward)
+          (goto-char (mark))
+          (isearch-repeat-forward)))
+    ad-do-it))
