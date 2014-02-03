70e0bca2f7e12ef43c18140794c49dfb2f0d849f
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-02-03 17:48:46 +0900

    backward-kill-word by C-w in helm-mode-map

--- .emacs.d/inits/11-helm.el
+++ .emacs.d/inits/11-helm.el
@@ -4,5 +4,6 @@
   '(progn
      (helm-descbinds-mode)
 
+     (define-key helm-map (kbd "C-w") 'backward-kill-word)
      (define-key helm-map (kbd "C-M-n") 'helm-next-source)
      (define-key helm-map (kbd "C-M-p") 'helm-previous-source)))
