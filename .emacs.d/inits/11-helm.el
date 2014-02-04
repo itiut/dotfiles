ee5509b8417b55b704906f29288e073fc6fe6cdb
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-02-05 02:24:17 +0900

    use-show-abs-or-relative-by-default in helm-ls-git-ls

--- .emacs.d/inits/11-helm.el
+++ .emacs.d/inits/11-helm.el
@@ -1,8 +1,5 @@
 ;;;; helm setting
 
-;; helm-ls-git
-(custom-set-variables '(helm-ls-git-show-abs-or-relative 'relative))
-
 (eval-after-load 'helm
   '(progn
      (helm-descbinds-mode)
@@ -10,3 +7,13 @@
      (define-key helm-map (kbd "C-w") 'backward-kill-word)
      (define-key helm-map (kbd "C-M-n") 'helm-next-source)
      (define-key helm-map (kbd "C-M-p") 'helm-previous-source)))
+
+
+;; helm-ls-git
+(custom-set-variables '(helm-ls-git-show-abs-or-relative 'relative))
+
+(eval-after-load 'helm-ls-git
+  '(progn
+     (defadvice helm-ls-git-ls (around use-show-abs-or-relative-by-default activate)
+       (let ((helm-ff-transformer-show-only-basename nil))
+         ad-do-it))))
