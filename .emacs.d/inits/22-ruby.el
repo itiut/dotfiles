7ef4a8a867ab53c2d9dea272f1fdda1a4597c141
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-02-04 23:37:33 +0900

    my/eval-after-load-ruby-mode-common

--- .emacs.d/inits/22-ruby.el
+++ .emacs.d/inits/22-ruby.el
@@ -4,43 +4,48 @@
 (add-to-list 'auto-mode-alist '("\\.ru\\'" . enh-ruby-mode))
 (add-to-list 'auto-mode-alist '("\\Gemfile\\'" . enh-ruby-mode))
 
-;; ruby-mode
-(custom-set-variables '(ruby-deep-indent-paren nil)
-                      '(ruby-deep-indent-paren-style nil)
-                      '(ruby-insert-encoding-magic-comment nil))
+(custom-set-variables
+ ;; ruby-mode
+ '(ruby-deep-indent-paren nil)
+ '(ruby-deep-indent-paren-style nil)
+ '(ruby-insert-encoding-magic-comment nil)
 
-(defadvice ruby-indent-line (after unindent-closing-paren activate)
-  (let ((column (current-column))
-        indent offset)
-    (save-excursion
-      (back-to-indentation)
-      (let ((state (syntax-ppss)))
-        (setq offset (- column (current-column)))
-        (when (and (or (eq (char-after) ?\))
-                       (and (eq (char-after) ?\})
-                            (eq (char-after (1+ (point))) ?\))))
-                   (not (zerop (car state))))
-          (goto-char (cadr state))
-          (setq indent (current-indentation)))))
-    (when indent
-      (indent-line-to indent)
-      (when (> offset 0)
-        (forward-char offset)))))
+ ;; enh-ruby-mode
+ '(enh-ruby-deep-indent-paren nil)
 
-;; enh-ruby-mode
-(custom-set-variables '(enh-ruby-deep-indent-paren nil))
+ ;; rsense
+ '(rsense-home (expand-file-name "rsense" my/site-lisp-directory))
 
-(defun my/eval-after-load-ruby-mode ()
-  ;; rsense
+ ;; ruby-end
+ '(ruby-end-insert-newline nil))
+
+(defun my/eval-after-load-ruby-mode-common ()
   (require 'rsense)
-  (custom-set-variables '(rsense-home (expand-file-name "rsense" my/site-lisp-directory)))
+  (require 'ruby-end))
 
-  ;; ruby-end
-  (require 'ruby-end)
-  (custom-set-variables '(ruby-end-insert-newline nil)))
+(eval-after-load 'ruby-mode
+  '(progn
+     (my/eval-after-load-ruby-mode-common)
 
-(eval-after-load 'ruby-mode '(my/eval-after-load-ruby-mode))
-(eval-after-load 'enh-ruby-mode '(my/eval-after-load-ruby-mode))
+     (defadvice ruby-indent-line (after unindent-closing-paren activate)
+       (let ((column (current-column))
+             indent offset)
+         (save-excursion
+           (back-to-indentation)
+           (let ((state (syntax-ppss)))
+             (setq offset (- column (current-column)))
+             (when (and (or (eq (char-after) ?\))
+                            (and (eq (char-after) ?\})
+                                 (eq (char-after (1+ (point))) ?\))))
+                        (not (zerop (car state))))
+               (goto-char (cadr state))
+               (setq indent (current-indentation)))))
+         (when indent
+           (indent-line-to indent)
+           (when (> offset 0)
+             (forward-char offset)))))))
+
+(eval-after-load 'enh-ruby-mode '(my/eval-after-load-ruby-mode-common))
 
 (defun my/ruby-mode-hook ()
   (add-to-list 'ac-sources 'ac-source-rsense)
