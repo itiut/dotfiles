0962cb0bdc75c880a55399df8931d931ed0788b5
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-02-05 23:43:16 +0900

    tabbar-ruler face

--- .emacs.d/inits/98-theme.el
+++ .emacs.d/inits/98-theme.el
@@ -8,15 +8,29 @@
 ;; monokai
 (load-theme 'monokai t)
 
-(let ((monokai-fg (face-foreground 'default))
-      (monokai-bg (face-background 'default))
-      (monokai-hi-line (face-background 'hl-line))
-      (monokai-comments (face-foreground 'shadow)))
+(defvar my/fg-color (face-foreground 'default))
+(defvar my/bg-color (face-background 'default))
+(defvar my/comments-color (face-foreground 'shadow))
+(defvar my/hl-line-color (face-background 'hl-line))
 
-  ;; yalinum
-  (set-face-attribute 'yalinum-face nil
-                      :foreground monokai-comments
-                      :background monokai-bg)
-  (set-face-attribute 'yalinum-bar-face nil
-                      :foreground monokai-fg
-                      :background monokai-hi-line))
+;; tabbar-ruler
+(eval-after-load 'tabbar-ruler
+  '(progn
+     (set-face-attribute 'tabbar-selected nil
+                         :foreground (face-foreground 'mode-line-buffer-id)
+                         :background my/bg-color)
+     (set-face-attribute 'tabbar-unselected nil
+                         :foreground my/fg-color
+                         :background my/hl-line-color)
+     (set-face-attribute 'tabbar-unselected-modified nil
+                         :foreground (face-foreground 'error))))
+
+;; yalinum
+(eval-after-load 'yalinum
+  '(progn
+     (set-face-attribute 'yalinum-face nil
+                         :foreground my/comments-color
+                         :background my/bg-color)
+     (set-face-attribute 'yalinum-bar-face nil
+                         :foreground my/fg-color
+                         :background my/hl-line-color)))
