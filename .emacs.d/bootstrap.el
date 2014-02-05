6cbbe2873efcfa9c79971080c4c4c72ce6b14e10
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-02-05 11:36:30 +0900

    disable byte-compile

--- .emacs.d/bootstrap.el
+++ .emacs.d/bootstrap.el
@@ -53,9 +53,6 @@
     recentf-ext
 
     ;;; programming
-    ;; Emacs Lisp
-    auto-async-byte-compile
-
     ;; Ruby
     enh-ruby-mode
     inf-ruby
@@ -76,11 +73,6 @@
 (dolist (package my/packages)
   (unless (package-installed-p package)
     (package-install package)))
-
-;; byte compile Emacs Lisp files
-(require 'init-loader)
-(byte-recompile-directory init-loader-directory 0)
-(byte-recompile-file (expand-file-name "init.el" user-emacs-directory) nil 0)
 
 ;;; user site-lisp
 (defvar my/site-lisp-directory
