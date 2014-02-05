19003d2b0c1f8d021e046e5249071a9a0a57e6e0
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-02-05 23:15:02 +0900

    enable flycheck in emacs-lisp-mode

--- .emacs.d/inits/13-flymake.el
+++ .emacs.d/inits/13-flymake.el
@@ -1,9 +1,13 @@
 ;;;; flymake setting
 
 ;; flycheck
-(custom-set-variables '(flycheck-display-errors-delay 0.5))
+(custom-set-variables '(flycheck-display-errors-delay 0.5)
+                      '(flycheck-disabled-checkers '(
+                                                     emacs-lisp-checkdoc
+                                                     )))
 
 (defvar my/flycheck-enable-modes '(
+                                   emacs-lisp-mode
                                    enh-ruby-mode
                                    js2-mode
                                    ruby-mode
