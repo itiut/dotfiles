10120e98c58739056214d3330ebeadb9a9d6c581
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-02-06 02:26:12 +0900

    ignore direx in tabbar

--- .emacs.d/inits/14-tabbar.el
+++ .emacs.d/inits/14-tabbar.el
@@ -5,12 +5,16 @@
      (defvar my/tabbar-ignored-buffer-names
        '("*Helm" "*helm"))
 
+     (defvar my/tabbar-ignored-buffer-major-modes
+       '(direx:direx-mode))
+
      (defun my/tabbar-buffer-list ()
        (let ((re (regexp-opt my/tabbar-ignored-buffer-names)))
          (delq nil
                (mapcar #'(lambda (b)
                            (cond
                             ((string-match re (buffer-name b)) nil)
+                            ((memq (buffer-local-value 'major-mode b) my/tabbar-ignored-buffer-major-modes) nil)
                             (t b)))
                        (tabbar-buffer-list)))))
 
