cda445f9445d049df177c27efa91819380c43ef1
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-02-13 05:51:18 +0900

    run ghpreview

--- .emacs.d/inits/31-markdown.el
+++ .emacs.d/inits/31-markdown.el
@@ -1,3 +1,16 @@
 ;;;; Markdown mode setting
 
 (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
+
+(eval-after-load 'markdown-mode
+  '(progn
+     (when (= (call-process-shell-command "which" nil nil nil "ghpreview") 0)
+       (defun my/run-ghpreview ()
+         (interactive)
+         (if (not buffer-file-name)
+             (error "Must be visiting a file")
+           (start-process-shell-command "ghpreview" nil
+                                        (format "ghpreview %s" buffer-file-name)))
+         (message "run ghpreview!"))
+
+       (define-key markdown-mode-map (kbd "C-c C-c p") 'my/run-ghpreview))))
