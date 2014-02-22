3b5e4d650d3ccbfdc1666455a000aa69c0b91510
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-02-22 16:54:09 +0900

    sort

--- .emacs.d/inits/99-keybinding.el
+++ .emacs.d/inits/99-keybinding.el
@@ -14,10 +14,10 @@
 (global-set-key (kbd "C-t") 'my/smart-other-window)
 (global-set-key (kbd "C-w") 'my/kill-region-or-backward-kill-word)
 (global-set-key (kbd "C-\\") 'my/cd-on-guake)
-(global-set-key (kbd "C-x K") 'kill-this-buffer)
-(global-set-key (kbd "C-x C-j") 'my/dired-jump)
 (global-set-key (kbd "C-S-k") 'kill-whole-line)
 (global-set-key (kbd "C-S-o") 'my/open-line-above)
+(global-set-key (kbd "C-x K") 'kill-this-buffer)
+(global-set-key (kbd "C-x C-j") 'my/dired-jump)
 
 ;; helm
 (global-set-key (kbd "C-x a") 'helm-apropos)
