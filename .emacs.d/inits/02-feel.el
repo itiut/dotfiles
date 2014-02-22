979b713b48908549b697261d5600fae3ed5ca82d
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-02-22 16:16:03 +0900

    extract my utilities

--- .emacs.d/inits/02-feel.el
+++ .emacs.d/inits/02-feel.el
@@ -19,57 +19,6 @@
 (require 'smartrep)
 (custom-set-variables '(smartrep-mode-line-active-bg nil))
 
-;;; functions
-(defun my/smart-beginning-of-line (old-point)
-  "Move point to the first non-whitespace character or beginning-of-line."
-  (interactive "d")
-  (back-to-indentation)
-  (when (= old-point (point))
-    (beginning-of-line)))
-
-(defun my/smart-other-window ()
-  "Split window if no more than one window exists, and move other window."
-  (interactive)
-  (if (one-window-p)
-      (progn
-        (if (< (frame-pixel-width) (frame-pixel-height))
-            (split-window-vertically)
-          (split-window-horizontally))
-        (other-window 1)
-        (switch-to-buffer (get-buffer (car (helm-buffer-list)))))
-    (other-window 1)))
-
-(defun my/kill-region-or-backward-kill-word ()
-  "Kill region if region is active and not empty. Otherwise kill a word just before point."
-  (interactive)
-  (if (use-region-p)
-      (kill-region (region-beginning) (region-end))
-    (backward-kill-word 1)))
-
-(defun my/open-line-above ()
-  "Open a line above the cursor, like 'O' in Vim."
-  (interactive)
-  (beginning-of-line)
-  (newline-and-indent)
-  (forward-line -1)
-  (indent-according-to-mode))
-
-(defun my/execute-on-guake (command)
-  "Execute `command' on Guake Terminal."
-  (interactive "MCommand: ")
-  (start-process-shell-command "guake" nil (format "guake -e '%s'" command)))
-
-(defun my/toggle-guake ()
-  "Toggle visibility of Guake Terminal."
-  (interactive)
-  (start-process-shell-command "guake" nil "guake -t"))
-
-(defun my/cd-on-guake ()
-  "Move to current directory in Guake Terminal, then show Guake Terminal."
-  (interactive)
-  (my/execute-on-guake (format "cd %s" default-directory))
-  (my/toggle-guake))
-
 
 ;;; advices
 ;; region based isearch when region is active
