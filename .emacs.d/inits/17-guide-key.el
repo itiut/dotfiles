9ad215c8ad144eab7ee1818d4772bc734e4abfbf
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-03-18 18:52:14 +0900

    Add guide-key setting for ghm-mode

--- .emacs.d/inits/17-guide-key.el
+++ .emacs.d/inits/17-guide-key.el
@@ -1,8 +1,17 @@
 ;;;; guide-key setting
 
 (guide-key-mode 1)
-(custom-set-variables '(guide-key/guide-key-sequence '("C-x r"
-                                                       (flycheck-mode "C-c !")))
-                      '(guide-key/highlight-command-regexp "rectangle")
+(custom-set-variables '(guide-key/guide-key-sequence
+                        '("C-x r"
+                          (flycheck-mode "C-c !")
+                          (gfm-mode "C-c"
+                                    "C-c C-a"
+                                    "C-c C-c"
+                                    "C-c C-s"
+                                    "C-c C-t"
+                                    "C-c C-x")))
+                      '(guide-key/highlight-command-regexp
+                        (regexp-opt '("rectangle"
+                                      "markdown")))
                       '(guide-key/popup-window-position 'bottom)
                       '(guide-key/text-scale-amount -0.25))
