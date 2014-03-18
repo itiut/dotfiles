7fbede7f7c13250f4fcbe09ee9beff6fd7fb6423
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-03-18 17:32:30 +0900

    my/development-directory

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -11,9 +11,14 @@
 (defvar my/site-lisp-directory
   (expand-file-name "site-lisp" user-emacs-directory)
   "user directory for site-lisp")
+(defvar my/development-directory
+  (expand-file-name "development" user-emacs-directory)
+  "user directory for development")
 
 ;; load-path
 (add-to-list 'load-path (expand-file-name "rsense/etc" my/site-lisp-directory))
+(let ((default-directory my/development-directory))
+  (normal-top-level-add-subdirs-to-load-path))
 
 ;; init-loader
 (require 'init-loader)
