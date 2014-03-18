977ae8267c43a4d62c128283b9bfcce97ffe3d2a
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-03-18 17:32:09 +0900

    comments

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -8,10 +8,11 @@
 ;; exec-path-from-shell
 (exec-path-from-shell-initialize)
 
-;; user site-lisp
 (defvar my/site-lisp-directory
-  (expand-file-name "site-lisp" user-emacs-directory))
+  (expand-file-name "site-lisp" user-emacs-directory)
+  "user directory for site-lisp")
 
+;; load-path
 (add-to-list 'load-path (expand-file-name "rsense/etc" my/site-lisp-directory))
 
 ;; init-loader
