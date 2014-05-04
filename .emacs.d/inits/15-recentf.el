4dc489ff666dd95136bc197a6166b17a50797191
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-05-05 02:14:57 +0900

    Ignore .recentf in recentf-mode

--- .emacs.d/inits/15-recentf.el
+++ .emacs.d/inits/15-recentf.el
@@ -2,6 +2,7 @@
 
 (custom-set-variables '(recentf-max-saved-items 1000)
                       '(recentf-auto-cleanup 600)
+                      '(recentf-exclude '("\\.recentf\\'"))
                       '(recentf-save-file (expand-file-name ".recentf" user-emacs-directory)))
 
 (run-with-idle-timer 30 t 'recentf-save-list)
