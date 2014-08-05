43ed2a31e9f644769c20b09007c7cfd8bffc6232
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-08-05 18:15:36 +0900

    Change font height
    110 ->
    140

--- .emacs.d/inits/cocoa-emacs-setting.el
+++ .emacs.d/inits/cocoa-emacs-setting.el
@@ -1,5 +1,10 @@
 ;;;; Cocoa Emacs setting
 
+;; font
+(set-face-attribute 'default nil
+                    :family "Ricty for Powerline"
+                    :height 140)
+
 ;; workaround for cute japanese font
 (set-fontset-font nil 'japanese-jisx0208
                   (font-spec :family "Ricty for Powerline"))
