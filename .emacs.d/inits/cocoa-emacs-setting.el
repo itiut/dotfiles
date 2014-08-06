c08a2418a73441f39f8f0acf02a9b45ecae3ef83
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-08-06 10:01:10 +0900

    Assign Meta to command key, Super to option key

--- .emacs.d/inits/cocoa-emacs-setting.el
+++ .emacs.d/inits/cocoa-emacs-setting.el
@@ -8,3 +8,7 @@
 ;; workaround for cute japanese font
 (set-fontset-font nil 'japanese-jisx0208
                   (font-spec :family "Ricty for Powerline"))
+
+;; assign Meta to command key, Super to option key
+(setq mac-command-modifier 'meta
+      mac-option-modifier 'super)
