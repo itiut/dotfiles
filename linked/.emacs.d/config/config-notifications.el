e38540f08097757eedcdb5fcea0223b5a610fbbd
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-09-07 21:38:02 +0900

    Remove old configs

--- linked/.emacs.d/config/config-notifications.el
+++
@@ -1,6 +0,0 @@
-;;; notifications
-(require 'notifications)
-
-(add-hook 'after-revert-hook
-          '(lambda ()
-             (notifications-notify :title "file reverted" :body buffer-file-name :urgency 'normal)))
