0605b8f47764a09018c957759180243d4a9ca155
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-09-08 00:06:16 +0900

    Combine directory development into site-lisp directory

--- linked/.emacs.d/init.el
+++ linked/.emacs.d/init.el
@@ -7,17 +7,13 @@
 ;; exec-path-from-shell
 (exec-path-from-shell-initialize)
 
-;; (defvar my/site-lisp-directory
-;;   (expand-file-name "site-lisp" user-emacs-directory)
-;;   "user directory for site-lisp")
-;; (defvar my/development-directory
-;;   (expand-file-name "development" user-emacs-directory)
-;;   "user directory for development")
+(defvar my/site-lisp-directory
+  (expand-file-name "site-lisp" user-emacs-directory)
+  "user directory for site-lisp")
 
-;; ;; load-path
-;; (add-to-list 'load-path (expand-file-name "rsense/etc" my/site-lisp-directory))
-;; (let ((default-directory my/development-directory))
-;;   (normal-top-level-add-subdirs-to-load-path))
+;; load-path
+(let ((default-directory my/site-lisp-directory))
+  (normal-top-level-add-subdirs-to-load-path))
 
 ;; init-loader
 (custom-set-variables '(init-loader-show-log-after-init 'error-only))
