2f9db99b3da540cf0d4d928f87ddd8a76fbb7897
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-09-13 21:00:44 +0900

    enh-ruby-mode has been already removed

--- linked/.emacs.d/inits/12-auto-complete.el
+++ linked/.emacs.d/inits/12-auto-complete.el
@@ -5,7 +5,3 @@
 
 (custom-set-variables '(ac-use-menu-map t)
                       '(ac-quick-help-delay 0.25))
-
-;; enable auto-complete mode other than default enable modes
-(dolist (mode '(enh-ruby-mode))
-  (add-to-list 'ac-modes mode))
