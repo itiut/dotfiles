c30f419121d87dea8c19a510a2588e34227bb948
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-09-13 16:31:15 +0900

    Fix regexp

--- linked/.emacs.d/inits/21-elisp.el
+++ linked/.emacs.d/inits/21-elisp.el
@@ -1,6 +1,6 @@
 ;;;; Emacs Lisp mode setting
 
-(add-to-list 'auto-mode-alist '("\\Cask\\'" . emacs-lisp-mode))
+(add-to-list 'auto-mode-alist '("Cask\\'" . emacs-lisp-mode))
 
 ;; eldoc
 (custom-set-variables '(eldoc-minor-mode-string ""))
--- linked/.emacs.d/inits/22-ruby.el
+++ linked/.emacs.d/inits/22-ruby.el
@@ -2,8 +2,8 @@
 
 (add-to-list 'auto-mode-alist '("\\.gemspec\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\Gemfile\\'" . ruby-mode))
-(add-to-list 'auto-mode-alist '("\\Rakefile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile\\'" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile\\'" . ruby-mode))
 
 (custom-set-variables
  ;; ruby-mode
