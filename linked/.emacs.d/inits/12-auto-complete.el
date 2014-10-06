4694e660c6868934355d6232ecf659e86f1dd2d0
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-10-06 19:21:50 +0900

    ac-config-default is autoloaded

--- linked/.emacs.d/inits/12-auto-complete.el
+++ linked/.emacs.d/inits/12-auto-complete.el
@@ -1,7 +1,5 @@
 ;;;; auto-complete setting
-(require 'auto-complete-config)
-
-(ac-config-default)
 
 (custom-set-variables '(ac-use-menu-map t)
                       '(ac-quick-help-delay 0.25))
+(ac-config-default)
