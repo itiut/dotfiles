aabf0f0f5942e5e4ffe21bc5f8357482c5fc4ec4
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2014-12-05 21:47:50 +0900

    Remove an unused setting in markdown-mode

--- linked/.emacs.d/inits/31-markdown.el
+++ linked/.emacs.d/inits/31-markdown.el
@@ -1,16 +1,3 @@
 ;;;; Markdown mode setting
 
 (add-to-list 'auto-mode-alist '("\\.md\\'" . gfm-mode))
-
-(eval-after-load 'markdown-mode
-  '(progn
-     (when (executable-find "ghpreview")
-       (defun my/run-ghpreview ()
-         (interactive)
-         (if (not buffer-file-name)
-             (error "Must be visiting a file")
-           (start-process-shell-command "ghpreview" nil
-                                        (format "ghpreview %s" buffer-file-name)))
-         (message "run ghpreview!"))
-
-       (define-key markdown-mode-map (kbd "C-c C-c p") 'my/run-ghpreview))))
