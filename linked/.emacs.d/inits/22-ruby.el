5f30a959bcfff6f8aa99e6d60def8e5d097b790e
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2015-05-11 04:18:56 +0900

    Use ruby-mode, ruby-end as default setting

--- linked/.emacs.d/inits/22-ruby.el
+++ linked/.emacs.d/inits/22-ruby.el
@@ -1,17 +1,6 @@
 ;;;; Ruby mode setting
 
-(custom-set-variables
- ;; ruby-mode
- '(ruby-deep-indent-paren nil)
- '(ruby-deep-indent-paren-style nil)
- '(ruby-insert-encoding-magic-comment nil)
-
- ;; ruby-end
- '(ruby-end-insert-newline nil))
-
 (with-eval-after-load 'ruby-mode
-  (require 'ruby-end)
-
   ;; robe
   (add-hook 'robe-mode-hook 'ac-robe-setup)
   (add-hook 'ruby-mode-hook 'robe-mode))
