c3e3e468cf400523fd704c81420848a0bca6cf4a
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2015-05-11 03:56:04 +0900

    Move whitespace setting into programming mode

--- linked/.emacs.d/inits/20-programming.el
+++ linked/.emacs.d/inits/20-programming.el
@@ -1,6 +1,34 @@
 ;;;; programming mode setting
 
+(custom-set-variables
+ ;; whitespace
+ '(whitespace-style '(
+                      ;; do not visualize by fase because of incompatibility with auto-complete
+                      trailing          ; spaces on EOL
+                      tabs
+                      spaces
+                      empty             ; empty lines on BOF/EOF
+                      space-mark        ; use display mapping
+                      tab-mark
+                      ))
+
+ '(whitespace-display-mappings
+   '((space-mark ?\u3000 [?\u25a1])
+     ;; WARNING: the mapping below has a problem.
+     ;; When a TAB occupies exactly one column, it will display the
+     ;; character ?\xBB at that column followed by a TAB which goes to
+     ;; the next TAB column.
+     ;; If this is a problem for you, please, comment the line below.
+     (tab-mark ?\t [?\u00BB ?\t] [?\\ ?\t])))
+
+ ;; concern only about full-width space
+ '(whitespace-space-regexp "\\(\u3000+\\)")
+
+ ;; auto cleanup before saved
+ '(whitespace-action '(auto-cleanup)))
+
 (defun my/prog-mode-hook ()
+  (whitespace-mode +1)
   (rainbow-delimiters-mode +1))
 
 (add-hook 'prog-mode-hook 'my/prog-mode-hook)
