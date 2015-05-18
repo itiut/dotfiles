bc9b7add913961ca43f0743bab073b2736edb877
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2015-05-18 13:12:30 +0900

    Apply helm-gtags-next/previous-history to smartrep-define-key

--- linked/.emacs.d/inits/11-helm.el
+++ linked/.emacs.d/inits/11-helm.el
@@ -15,8 +15,9 @@
   (define-key helm-gtags-mode-map (kbd "M-t") 'helm-gtags-find-tag)
   (define-key helm-gtags-mode-map (kbd "M-r") 'helm-gtags-find-rtag)
   (define-key helm-gtags-mode-map (kbd "M-s") 'helm-gtags-find-symbol)
-  (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
-  (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+  (smartrep-define-key
+      helm-gtags-mode-map "C-c" '((">" . 'helm-gtags-next-history)
+                                  ("<" . 'helm-gtags-previous-history)))
   (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack))
 
 ;; helm-ls-git-ls
