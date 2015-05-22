12c88a9011bd5ce42b2861e79bc70ed0912f6560
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2015-05-22 17:19:27 +0900

    Use python3 as a default interpreter

--- linked/.emacs.d/inits/22-python.el
+++ linked/.emacs.d/inits/22-python.el
@@ -1,6 +1,16 @@
 ;;;; Python mode setting
 
+(custom-set-variables '(python-shell-interpreter "python3"))
+
+(with-eval-after-load 'python-environment
+  (custom-set-variables '(python-environment-virtualenv
+                          (append python-environment-virtualenv '("--python" "python3")))))
+
 ;; jedi
 (custom-set-variables '(jedi:complete-on-dot t)
                       '(jedi:tooltip-method nil))
 (add-hook 'python-mode-hook 'jedi:setup)
+
+;; quickrun
+(quickrun-add-command "python"
+                      '((:command . "python3")) :override t)
