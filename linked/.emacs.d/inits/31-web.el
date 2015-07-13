61aa928478195306d0aa582ebc047d4b6141a240
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2015-07-13 16:23:31 +0900

    Fix indentation in web-mode

--- linked/.emacs.d/inits/31-web.el
+++ linked/.emacs.d/inits/31-web.el
@@ -3,5 +3,6 @@
 (add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 
-(custom-set-variables '(web-mode-script-padding 2)
-                      '(web-mode-style-padding 2))
+(custom-set-variables '(web-mode-markup-indent-offset 2)
+                      '(web-mode-css-indent-offset 2)
+                      '(web-mode-code-indent-offset 2))
