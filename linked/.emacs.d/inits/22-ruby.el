bcf64a4099d8161b390abca4a0dbc7a55176e08d
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2015-07-29 17:46:23 +0900

    Disable inserting magic comments

--- linked/.emacs.d/inits/22-ruby.el
+++ linked/.emacs.d/inits/22-ruby.el
@@ -1,5 +1,7 @@
 ;;;; Ruby mode setting
 
+(custom-set-variables '(ruby-insert-encoding-magic-comment nil))
+
 (with-eval-after-load 'ruby-mode
   ;; robe
   (add-hook 'robe-mode-hook 'ac-robe-setup)
