11e418169f7eb5ec8c2ad134f0255edf447f6f72
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2015-08-05 17:14:55 +0900

    Fix initializing recentf-exclude
    git-commit.el cannot set recentf-exclude properly

--- linked/.emacs.d/inits/15-recentf.el
+++ linked/.emacs.d/inits/15-recentf.el
@@ -3,11 +3,11 @@
 (recentf-mode 1)
 (custom-set-variables '(recentf-max-saved-items 1000)
                       '(recentf-auto-cleanup 600)
-                      '(recentf-exclude '("\\.recentf\\'"))
                       '(recentf-save-file (expand-file-name ".recentf" user-emacs-directory)))
 
 (run-with-idle-timer 30 t '(lambda ()
                              (my/with-suppressed-message (recentf-save-list))))
 
 (with-eval-after-load 'recentf
+  (add-to-list 'recentf-exclude '("\\.recentf\\'"))
   (require 'recentf-ext))
