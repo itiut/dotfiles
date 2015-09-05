71926fe98961843a73dab15e37dc19f36bbbe37f
Yuichi TANIKAWA <t.yuichi111@gmail.com>
2015-09-05 11:32:04 +0900

    Install json-mode

--- linked/.emacs.d/inits/22-javascript.el
+++ linked/.emacs.d/inits/22-javascript.el
@@ -3,4 +3,5 @@
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 (custom-set-variables '(coffee-tab-width 2)
+                      '(js-indent-level 2)
                       '(js2-basic-offset 2))
