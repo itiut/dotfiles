7db68ce2370abecda795f0a396b158a8747aee46
Yuichi Tanikawa <t.yuichi111@gmail.com>
2015-10-18 12:30:03 +0900

    Set web-mode-scrint-padding and -style-padding to 2

--- linked/.emacs.d/inits/31-web.el
+++ linked/.emacs.d/inits/31-web.el
@@ -4,9 +4,11 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 
 (custom-set-variables '(css-indent-offset 2)
-                      '(web-mode-markup-indent-offset 2)
+                      '(web-mode-code-indent-offset 2)
                       '(web-mode-css-indent-offset 2)
-                      '(web-mode-code-indent-offset 2))
+                      '(web-mode-markup-indent-offset 2)
+                      '(web-mode-script-padding 2)
+                      '(web-mode-style-padding 2))
 
 (add-hook 'css-mode-hook 'emmet-mode)
 (add-hook 'css-mode-hook 'rainbow-mode)
