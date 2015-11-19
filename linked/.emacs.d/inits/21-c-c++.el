9e3624b867a80d2afe0f355d72dfbef62e897497
Yuichi Tanikawa <t.yuichi111@gmail.com>
2015-11-19 13:26:39 +0900

    Quick fix: not allow php-mode-hook to run my/c*-mode-hook

--- linked/.emacs.d/inits/21-c-c++.el
+++ linked/.emacs.d/inits/21-c-c++.el
@@ -1,27 +1,29 @@
 ;;;; C/C++ mode setting
 
 (defun my/c-c++-mode-hook ()
-  (c-set-style "stroustrup")
-  (c-set-offset 'innamespace 0)
-  (c-set-offset 'arglist-close 0)
+  (unless (eq major-mode 'php-mode)
+    (c-set-style "stroustrup")
+    (c-set-offset 'innamespace 0)
+    (c-set-offset 'arglist-close 0)
 
-  ;; auto-complete-clang
-  (require 'auto-complete-clang)
-  (add-to-list 'ac-sources 'ac-source-clang)
-  (setq-local ac-delay 0.5)             ; Default value 0.1 is too fast.
+    ;; auto-complete-clang
+    (require 'auto-complete-clang)
+    (add-to-list 'ac-sources 'ac-source-clang)
+    (setq-local ac-delay 0.5)         ; Default value 0.1 is too fast.
 
-  ;; auto-complete-clang-async
-  ;; (when (executable-find "clang-complete")
-  ;;   (require 'auto-complete-clang-async)  ; TODO: autoload?
-  ;;   (add-to-list 'ac-sources 'ac-source-clang-async)
-  ;;   (ac-clang-launch-completion-process))
+    ;; auto-complete-clang-async
+    ;; (when (executable-find "clang-complete")
+    ;;   (require 'auto-complete-clang-async)  ; TODO: autoload?
+    ;;   (add-to-list 'ac-sources 'ac-source-clang-async)
+    ;;   (ac-clang-launch-completion-process))
 
-  ;; c-eldoc
-  (c-turn-on-eldoc-mode))
+    ;; c-eldoc
+    (c-turn-on-eldoc-mode)))
 
 (defun my/c-mode-hook ()
-  (setq flycheck-clang-language-standard "c99")
-  (setq-local quickrun-option-cmdopt "-std=c99 -O2"))
+  (unless (eq major-mode 'php-mode)
+    (setq flycheck-clang-language-standard "c99")
+    (setq-local quickrun-option-cmdopt "-std=c99 -O2")))
 
 (defun my/c++-mode-hook ()
   (setq flycheck-clang-language-standard "c++11")
