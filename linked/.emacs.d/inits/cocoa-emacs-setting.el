f203dacd4dfed91ed15ff2b64d217d8123eb45cd
Yuichi Tanikawa <t.yuichi111@gmail.com>
2015-11-20 00:20:29 +0900

    Stop switching meta and super

--- linked/.emacs.d/inits/cocoa-emacs-setting.el
+++ linked/.emacs.d/inits/cocoa-emacs-setting.el
@@ -6,10 +6,6 @@
 ;; enable menu bar
 (menu-bar-mode +1)
 
-;; assign Meta to command key, Super to option key
-(setq mac-command-modifier 'meta
-      mac-option-modifier 'super)
-
 ;; type a backslash on a JIS keyboard
 (define-key global-map [165] [92])
 ;; use M-\ key binding on a JIS keyboard
