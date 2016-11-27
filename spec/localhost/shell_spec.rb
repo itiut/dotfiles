d93f045b3d864d508a733260614e89506a283b59
Yuichi Tanikawa <t.yuichi111@gmail.com>
2016-11-27 12:05:32 +0900

    Fix login shell test

--- spec/localhost/shell_spec.rb
+++ spec/localhost/shell_spec.rb
@@ -1,17 +1,17 @@
 require 'spec_helper'
 
 describe 'shell' do
-  describe user(Etc.getlogin) do
+  describe user(ENV['USER']) do
     it { should have_login_shell('/usr/local/bin/zsh') }
   end
 
   describe file(File.expand_path('.zgen/zgen.zsh', '~/')) do
     it { should be_file }
-    it { should be_executable.by_user(Etc.getlogin) }
+    it { should be_executable.by_user(ENV['USER']) }
   end
 
   describe file(File.expand_path('.tmux/plugins/tpm/tpm', '~/')) do
     it { should be_file }
-    it { should be_executable.by_user(Etc.getlogin) }
+    it { should be_executable.by_user(ENV['USER']) }
   end
 end
